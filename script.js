b3745d9cdb7352db389dca9be341f0e5c2c4beee
RUHAN K.B. <ruhankb29@gmail.com>
2025-09-28 20:11:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Particles.js configuration */
+particlesJS("particles-js", {
+    "particles": {
+        "number": { "value": 80 },
+        "color": { "value": ["#A9FEF7","#FFD700","#58a6ff"] },
+        "shape": { "type": "circle" },
+        "opacity": { "value": 0.5 },
+        "size": { "value": 3 },
+        "line_linked": { "enable": true, "distance": 150, "color": "#A9FEF7", "opacity": 0.4, "width": 1 },
+        "move": { "enable": true, "speed": 3 }
+    },
+    "interactivity": {
+        "events": { "onhover": { "enable": true, "mode": "repulse" } }
+    },
+    "retina_detect": true
+});
